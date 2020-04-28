bf78eed0a0d55190e2f188368d7a49c0538d9266
Keyper <kcybulski93@gmail.com>
2020-04-28 19:43:45 +0200

    Return to the previous version
    I will try to do it with buttons for each new page.

--- public/index.js
+++ public/index.js
@@ -18,30 +18,17 @@ fetch(url)
     })
     .then(response => response.json())
     .then(data => {
-        let count = data.count,
-            i = 0;
-        for (i = 1; i <= count + 1; i++) {
-            url2 = `https://swapi.dev/api/people/${i}/`
-            fetch(url2)
-                .then(response => {
-                    if (response.ok) {
-                        return response;
-                    }
-                    throw Error(response.status)
-                })
-                .then(response => response.json())
-                .then(character => {
-                    let div2 = createNode('div'),
-                        span = createNode('span'),
-                        a = createNode('a');
-                    div2.classList.add('character');
-                    span.innerHTML = `<i class="icon-star-1"> ${character.name}`;
-                    a.href = `character-details.html?id=${character.name}`;
-                    append(div1, div2);
-                    append(div2, a);
-                    append(a, span);
-                })
-                .catch(error => console.log(error))
-        }
+        let characters = data.results
+        return characters.map(character => {
+            let div2 = createNode('div'),
+                span = createNode('span'),
+                a = createNode('a');
+            div2.classList.add('character');
+            span.innerHTML = `<i class="icon-star-1"> ${character.name}`;
+            a.href = `character-details.html?id=${character.name}`;
+            append(div1, div2);
+            append(div2, a);
+            append(a, span);
+        })
     })
     .catch(error => console.log(error))
