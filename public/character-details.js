d88599b30f51a30dab3cb84b89b6f151f72a8aba
Keyper <kcybulski93@gmail.com>
2020-04-28 10:40:35 +0200

    Add all characters - first slow option

--- public/character-details.js
+++ public/character-details.js
@@ -13,20 +13,38 @@ fetch(url)
     })
     .then(response => response.json())
     .then(data => {
-        for (let i = 0, max = data.results.length; i < max; i++) {
-            if (myParam === data.results[i].name)
-                return data.results[i];
+        let count = data.count,
+            i = 0;
+        for (i = 1; i <= count + 1; i++) {
+            url2 = `https://swapi.dev/api/people/${i}/`
+            fetch(url2)
+                .then(response => {
+                    if (response.ok) {
+                        return response;
+                    }
+                    throw Error(response.status)
+                })
+                .then(response => response.json())
+                .then(character => {
+                    if (myParam === character.name) {
+                        return character
+                    }
+                })
+                .then(character => {
+                    if (character) {
+                        div.innerHTML =
+                            `<div class="text">Name:</div> <div class="data">${character.name}</div>
+                            <div class="text">Gender:</div> <div class="data">${character.gender}</div>
+                            <div class="text">Height:</div> <div class="data">${character.height}</div>
+                            <div class="text">Mass:</div> <div class="data">${character.mass}</div>
+                            <div class="text">Birth year:</div> <div class="data">${character.birth_year}</div>
+                            <div class="text">Hair color:</div> <div class="data">${character.hair_color}</div>
+                            <div class="text">Skin color:</div> <div class="data">${character.skin_color}</div>
+                            <div class="text">Eye color:</div> <div class="data">${character.eye_color}</div>`
+                    } else 0
+                })
+                .catch(error => console.log(error))
+
         }
-    })
-    .then(character => {
-        div.innerHTML =
-            `<div class="text">Name:</div> <div class="data">${character.name}</div>
-            <div class="text">Gender:</div> <div class="data">${character.gender}</div>
-            <div class="text">Height:</div> <div class="data">${character.height}</div>
-            <div class="text">Mass:</div> <div class="data">${character.mass}</div>
-            <div class="text">Birth year:</div> <div class="data">${character.birth_year}</div>
-            <div class="text">Hair color:</div> <div class="data">${character.hair_color}</div>
-            <div class="text">Skin color:</div> <div class="data">${character.skin_color}</div>
-            <div class="text">Eye color:</div> <div class="data">${character.eye_color}</div>`
     })
     .catch(error => console.log(error))
--- public/index.js
+++ public/index.js
@@ -18,17 +18,30 @@ fetch(url)
     })
     .then(response => response.json())
     .then(data => {
-        let characters = data.results
-        return characters.map(character => {
-            let div2 = createNode('div'),
-                span = createNode('span'),
-                a = createNode('a');
-            div2.classList.add('character');
-            span.innerHTML = `<i class="icon-star-1"> ${character.name}`;
-            a.href = `character-details.html?id=${character.name}`;
-            append(div1, div2);
-            append(div2, a);
-            append(a, span);
-        })
+        let count = data.count,
+            i = 0;
+        for (i = 1; i <= count + 1; i++) {
+            url2 = `https://swapi.dev/api/people/${i}/`
+            fetch(url2)
+                .then(response => {
+                    if (response.ok) {
+                        return response;
+                    }
+                    throw Error(response.status)
+                })
+                .then(response => response.json())
+                .then(character => {
+                    let div2 = createNode('div'),
+                        span = createNode('span'),
+                        a = createNode('a');
+                    div2.classList.add('character');
+                    span.innerHTML = `<i class="icon-star-1"> ${character.name}`;
+                    a.href = `character-details.html?id=${character.name}`;
+                    append(div1, div2);
+                    append(div2, a);
+                    append(a, span);
+                })
+                .catch(error => console.log(error))
+        }
     })
     .catch(error => console.log(error))
