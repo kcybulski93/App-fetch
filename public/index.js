71a1d91b5940766e7edaa0e62c919d2e3f9aede4
Keyper <kcybulski93@gmail.com>
2020-04-29 10:10:07 +0200

    Add Pages mechanism

--- public/index.js
+++ public/index.js
@@ -6,8 +6,38 @@ function append(parent, el) {
     return parent.appendChild(el)
 }
 
-const div1 = document.getElementById('characters')
-const url = "https://swapi.dev/api/people/"
+function newPage() {
+    let txt = this.innerText;
+    let link = `http://swapi.dev/api/people/?page=${txt}`
+    divCharacters.innerHTML = null;
+    fetch(link)
+        .then(response => {
+            if (response.ok) {
+                return response;
+            }
+            throw Error(response.status)
+        })
+        .then(response => response.json())
+        .then(data => {
+            let characters = data.results
+            return characters.map(character => {
+                let div = createNode('div'),
+                    span = createNode('span'),
+                    a = createNode('a');
+                div.classList.add('character');
+                span.innerHTML = `<i class="icon-star-1">${character.name}`;
+                a.href = `character-details.html?id=${character.name}`;
+                append(divCharacters, div);
+                append(div, a);
+                append(a, span);
+            })
+        })
+        .catch(error => console.log(error))
+}
+
+const divCharacters = document.getElementById('characters');
+const url = "https://swapi.dev/api/people/";
+const divButtons = document.getElementById('buttons');
 
 fetch(url)
     .then(response => {
@@ -20,15 +50,29 @@ fetch(url)
     .then(data => {
         let characters = data.results
         return characters.map(character => {
-            let div2 = createNode('div'),
+            let div = createNode('div'),
                 span = createNode('span'),
                 a = createNode('a');
-            div2.classList.add('character');
+            div.classList.add('character');
             span.innerHTML = `<i class="icon-star-1"> ${character.name}`;
             a.href = `character-details.html?id=${character.name}`;
-            append(div1, div2);
-            append(div2, a);
+            append(divCharacters, div);
+            append(div, a);
             append(a, span);
         })
     })
     .catch(error => console.log(error))
+
+for (let i = 0; i < 9; i++) {
+    let btn = createNode('button');
+    btn.innerHTML = `${i+1}`;
+    btn.classList.add(`button`);
+    btn.id = `btn`;
+    append(divButtons, btn);
+}
+
+let button = document.querySelectorAll('button');
+
+for (let i = 0; i < 9; i++) {
+    button[i].addEventListener('click', newPage)
+}
